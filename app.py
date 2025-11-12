024be3512f23ffd3c128a5442fd94568046825aa
Vara-1 <adevivaraprasad6@gmail.com>
2025-11-12 09:09:18 +0530

    v2

--- app.py
+++ app.py
@@ -279,3 +279,8 @@ def deletenote(id):
 # -------------------- RUN APP --------------------
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY,username VARCHAR(100) NOT NULL,email VARCHAR(100) NOT NULL UNIQUE,password VARCHAR(255) NOT NULL);
+
+# CREATE TABLE notes (id INT AUTO_INCREMENT PRIMARY KEY,title VARCHAR(255) NOT NULL,content TEXT NOT NULL,user_id INT NOT NULL,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,FOREIGN KEY (user_id) REFERENCES students(id) ON DELETE CASCADE);
